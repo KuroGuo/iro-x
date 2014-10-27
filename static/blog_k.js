18f78fafbb0505975e14833c9f4d6bae660a3438
KuroGuo <413298956@qq.com>
2014-10-27 13:14:39 +0800

    修正主界面时间timeout为interval

--- static/blog_k.js
+++ static/blog_k.js
@@ -647,83 +647,95 @@ angular.module('blog_k', [
             }
         }
     };
-}]);;angular.module('blog_k.home', ['kplayer'])
-    .controller('HomeCtrl', ['$scope', '$state', '$timeout', function ($scope, $state, $timeout) {
-        $timeout(function () {
-            $scope.$apply(setDateTime);
-        }, 1000);
+}]);;;(function (angular) {
+    angular.module('blog_k.home', ['kplayer'])
+        .controller('HomeCtrl', [function () {
 
-        setDateTime();
+        }])
+        .directive('datetimeBox', ['$interval', function ($interval) {
+            return {
+                restrict: 'E',
+                link: function (scope, element, attrs, controller) {
+                    $interval(setDateTime, 1000);
 
-        function setDateTime() {
-            var now = new Date();
+                    setDateTime();
 
-            setTime(now);
-            setDate(now);
-            setDay(now);
-        }
+                    function setDateTime() {
+                        var now = new Date();
 
-        function setDate(now) {
-            var month = now.getMonth() + 1;
-            var date = now.getDate();
-            var day = now.getDay();
+                        scope.time = getTime(now);
+                        scope.date = getDate(now);
+                        scope.day = getDay(now);
+                    }
 
-            $scope.date = numToHanzi(month) + '月' + numToHanzi(date) + '日';
-        }
+                    function getDate(now) {
+                        var month = now.getMonth() + 1;
+                        var date = now.getDate();
+                        var day = now.getDay();
 
-        function setTime(now) {
-            var hours = now.getHours();
-            var minutes = now.getMinutes();
+                        return numToHanzi(month) + '月' + numToHanzi(date) + '日';
+                    }
 
-            $scope.time = hours + ':' + minutes;
-        }
+                    function getTime(now) {
+                        var hours = now.getHours();
+                        var minutes = now.getMinutes();
 
-        function setDay(now) {
-            var day = now.getDay();
+                        return hours + ':' + minutes;
+                    }
 
-            var dayHanzi = {
-                '0': '星期日',
-                '1': '星期一',
-                '2': '星期二',
-                '3': '星期三',
-                '4': '星期四',
-                '5': '星期五',
-                '6': '星期六'
-            };
+                    function getDay(now) {
+                        var day = now.getDay();
 
-            $scope.day = dayHanzi[day];
-        }
+                        var dayHanzi = {
+                            '0': '星期日',
+                            '1': '星期一',
+                            '2': '星期二',
+                            '3': '星期三',
+                            '4': '星期四',
+                            '5': '星期五',
+                            '6': '星期六'
+                        };
 
-        function numToHanzi(num) {
-            var tens = Math.floor(num / 10);
-            var units = Math.floor(num % 10);
+                        return dayHanzi[day];
+                    }
 
-            var numHanzi = {
-                '0': '',
-                '1': '一',
-                '2': '二',
-                '3': '三',
-                '4': '四',
-                '5': '五',
-                '6': '六',
-                '7': '七',
-                '8': '八',
-                '9': '九'
-            };
+                    function numToHanzi(num) {
+                        var tens = Math.floor(num / 10);
+                        var units = Math.floor(num % 10);
 
-            var result = '';
+                        var numHanzi = {
+                            '0': '',
+                            '1': '一',
+                            '2': '二',
+                            '3': '三',
+                            '4': '四',
+                            '5': '五',
+                            '6': '六',
+                            '7': '七',
+                            '8': '八',
+                            '9': '九'
+                        };
 
-            if (tens > 0) {
-                if (tens > 1) {
-                    result += numHanzi[tens];
+                        var result = '';
+
+                        if (tens > 0) {
+                            if (tens > 1) {
+                                result += numHanzi[tens];
+                            }
+                            result += '十';
+                        }
+                        result += numHanzi[units];
+
+                        return result;
+                    }
                 }
-                result += '十';
-            }
-            result += numHanzi[units];
+            };
 
-            return result;
-        }
-    }]);;angular.module('blog_k.login', ['ui.router'])
+            $interval(function () {
+                $scope.$apply(setDateTime);
+            }, 1000);
+        }]);
+})(angular);;angular.module('blog_k.login', ['ui.router'])
     .controller('LoginCtrl', ['$scope', '$state', 'member', function ($scope, $state, member) {
         $scope.submit = function () {
             var splited = $scope.nameandpassword.split('|'),
