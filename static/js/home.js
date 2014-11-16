d513ac6fcf002edcf05f8be74dcff93ac6cb5f28
KuroGuo <413298956@qq.com>
2014-11-16 11:58:10 +0800

    hour minutes width 2

--- static/js/home.js
+++ static/js/home.js
@@ -30,6 +30,13 @@
                         var hours = now.getHours();
                         var minutes = now.getMinutes();
 
+                        if (hours < 10) {
+                            hours = '0' + hours;
+                        }
+                        if (minutes < 10) {
+                            minutes + '0' + minutes;
+                        }
+
                         return hours + ':' + minutes;
                     }
 
