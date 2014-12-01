5b01c1db754a6d3046673c6dacf5e7393353c449
KuroGuo <413298956@qq.com>
2014-12-01 13:14:13 +0800

    壁纸li宽度自动调整优化

--- static/js/wallpaper.js
+++ static/js/wallpaper.js
@@ -1,10 +1,24 @@
 ;(function (angular) { 'use strict';
   angular.module('iro.wallpaper', ['iro.services.wallpaper', 'iro.services.navbar'])
-    .controller('WallpaperCtrl', ['$scope', 'wallpaper', 'navbar', function ($scope, wallpaper, navbar) {
-      $scope.wallpapers = wallpaper.query();
-      navbar.customBackgroundColor = 'rgba(51,51,51,1)';
-      $scope.$on('$destroy', function () {
-        navbar.customBackgroundColor = null;
-      });
-    }]);
+    .controller('WallpaperCtrl', ['$scope', 'wallpaper', 'navbar', '$window', '$document',
+      function ($scope, wallpaper, navbar, $window, $document) {
+        $scope.wallpapers = wallpaper.query();
+
+        $scope.liHeight = '16rem';
+        $window.addEventListener('resize', function () {
+          $scope.$apply(rewidth);
+        });
+        rewidth.call($window);
+
+        navbar.customBackgroundColor = 'rgba(51,51,51,1)';
+        $scope.$on('$destroy', function () {
+          $window.removeEventListener('resize', onResize);
+          navbar.customBackgroundColor = null;
+        });
+
+        function rewidth() {
+          var rootFontSize = parseFloat($document.find('html').css('font-size'));
+          $scope.liWidth = 100 / Math.round($window.innerWidth / rootFontSize / (parseFloat($scope.liHeight) * 16 / 9)) + '%';
+        }
+      }]);
 })(angular);
