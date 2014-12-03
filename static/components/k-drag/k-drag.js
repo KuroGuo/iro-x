c159d1bb704aae4ec878e45a92661cb36bde4c96
KuroGuo <413298956@qq.com>
2014-12-03 18:14:36 +0800

    多点触控

--- static/components/k-drag/k-drag.js
+++ static/components/k-drag/k-drag.js
@@ -4,30 +4,37 @@
       return {
         restrict: 'E',
         link: function (scope, element, attrs, controller) {
-          var state = 0; // 0: 初始状态, 1: 按下, 2: dragging
-          var pointerdownPageXY; // 按下时的PageXY
-          var pageXY; // 拖动时和拖动结束时的PageXY
-          var lastFramePageXY, lastMovePageXY, lastMoveTime, currentTime;
-          var vx, vy;
-          var target; // 拖动的目标element，非常重要！！！
-          var adsorb;
-          var touchIdentifier;
-
-          var animationFrameRequested = false;
-
           $document
-            .on('mousedown touchstart', function (e) {
-              if (state > 0)
-                return;
-              if (e.type === 'touchstart')
-                touchIdentifier = e.originalEvent.changedTouches[0].identifier;
-              lastFramePageXY = pageXY = pointerdownPageXY = getEventPageXY(e);
-              lastMoveTime = e.timeStamp;
-              target = e.target;
-              adsorb = parseFloat($(target).attr('k-drag-adsorb')) || 0;
-              state = 1;
-            })
-            .on('mousemove touchmove', function (e) {
+            .on('mousedown touchstart', dragStart);
+
+          function dragStart(e) {
+            var state = 0; // 0: 初始状态, 1: 按下, 2: dragging
+            var pointerdownPageXY; // 按下时的PageXY
+            var pageXY; // 拖动时和拖动结束时的PageXY
+            var lastFramePageXY, lastMovePageXY, lastMoveTime, currentTime;
+            var vx, vy;
+            var target; // 拖动的目标element，非常重要！！！
+            var adsorb;
+            var touchIdentifier;
+
+            var animationFrameRequested = false;
+            
+            if (e.type === 'touchstart')
+              touchIdentifier = e.originalEvent.changedTouches[0].identifier;
+            lastFramePageXY = pageXY = pointerdownPageXY = getEventPageXY(e);
+            lastMoveTime = e.timeStamp;
+            target = e.target;
+            adsorb = parseFloat($(target).attr('k-drag-adsorb')) || 0;
+            state = 1;
+
+            $document
+              .on('mousemove touchmove', drag)
+              .on('mouseup touchend', dragend);
+
+            $($window).on('blur', dragend);
+
+            function drag(e) {
+              console.log('drag');
               if (e.which && e.which !== 1) {
                 return;
               }
@@ -36,8 +43,9 @@
                 return;
               }
 
-              if (e.type === 'touchmove' && e.originalEvent.changedTouches[0].identifier !== touchIdentifier)
+              if (e.type === 'touchmove' && e.originalEvent.changedTouches[0].identifier !== touchIdentifier) {
                 return;
+              }
 
               lastMovePageXY = pageXY;
               pageXY = getEventPageXY(e);
@@ -92,33 +100,68 @@
                 });
                 animationFrameRequested = true;
               }
-            })
-            .on('mouseup touchend', cancelDrag);
-
-          $($window).on('blur', cancelDrag);
-
-          function cancelDrag(e) {
-            if (animationFrameRequested) {
-              $window.requestAnimationFrame(_do);
-            } else {
-              _do();
             }
 
-            function _do() {
-              if (e.type === 'touchend' && e.originalEvent.changedTouches[0].identifier !== touchIdentifier)
-                return;
-
-              var _event;
-              if (state === 2) {
-                if (e.timeStamp - lastMoveTime > 100) {
-                  vx = 0;
-                  vy = 0;
-                }
-
-                _event = newEvent('kdragend', e);
-                $(target).trigger(_event);
+            function dragend(e) {
+              if (animationFrameRequested) {
+                $window.requestAnimationFrame(_do);
+              } else {
+                _do();
               }
-              state = 0;
+
+              function _do() {
+                if (e.type === 'touchend' && e.originalEvent.changedTouches[0].identifier !== touchIdentifier)
+                  return;
+
+                var _event;
+                if (state === 2) {
+                  if (e.timeStamp - lastMoveTime > 100) {
+                    vx = 0;
+                    vy = 0;
+                  }
+
+                  _event = newEvent('kdragend', e);
+                  $(target).trigger(_event);
+                }
+                state = 0;
+
+                $document
+                  .off('mousemove touchmove', drag)
+                  .off('mouseup touchend', dragend);
+                $($window).off('blur', dragend);
+              }
+            }
+
+            function newEvent(name, e) {
+              var _event = $.Event(name);
+
+              pageXY = getEventPageXY(e);
+
+              _event.pageX = pageXY.x || -1;
+              _event.pageY = pageXY.y || -1;
+              _event.deltaX = (pageXY.x - pointerdownPageXY.x) || -1;
+              _event.deltaY = (pageXY.y - pointerdownPageXY.y) || -1;
+              _event.stepX = pageXY.x - lastFramePageXY.x;
+              _event.stepY = pageXY.y - lastFramePageXY.y;
+              _event.vx = vx || 0;
+              _event.vy = vy || 0;
+
+              if (e.type.indexOf('mouse') > -1) {
+                _event.pointerType = 'mouse';
+              } else if (e.type.indexOf('touch') > -1) {
+                _event.pointerType = 'touch';
+              } else {
+                _event.pointerType = e.type;
+              }
+              _event.preventDefault = function () {
+                e.preventDefault();
+              };
+              _event.prevent = function () {
+                state = 0;
+                dragend();
+              };
+
+              return _event;
             }
           }
 
@@ -135,37 +178,6 @@
             }
 
             return {x: pageX, y: pageY};
-          }
-
-          function newEvent(name, e) {
-            var _event = $.Event(name);
-
-            pageXY = getEventPageXY(e);
-
-            _event.pageX = pageXY.x || -1;
-            _event.pageY = pageXY.y || -1;
-            _event.deltaX = (pageXY.x - pointerdownPageXY.x) || -1;
-            _event.deltaY = (pageXY.y - pointerdownPageXY.y) || -1;
-            _event.stepX = pageXY.x - lastFramePageXY.x;
-            _event.stepY = pageXY.y - lastFramePageXY.y;
-            _event.vx = vx || 0;
-            _event.vy = vy || 0;
-
-            if (e.type.indexOf('mouse') > -1) {
-              _event.pointerType = 'mouse';
-            } else if (e.type.indexOf('touch') > -1) {
-              _event.pointerType = 'touch';
-            } else {
-              _event.pointerType = e.type;
-            }
-            _event.preventDefault = function () {
-              e.preventDefault();
-            };
-            _event.prevent = function () {
-              state = 0;
-            };
-
-            return _event;
           }
         }
       };
