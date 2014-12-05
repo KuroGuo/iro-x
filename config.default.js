2d4ef31a220502be21930536386c8ddb50232b80
KuroGuo <413298956@qq.com>
2014-12-05 20:15:36 +0800

    增加注释，增加爬虫间隔时间配置项

--- config.default.js
+++ config.default.js
@@ -2,10 +2,20 @@
 'use strict';
 
 var config = {
+  // mongodb地址
   db: 'mongodb://localhost/iro',
+
+  // 监听端口
   port: process.env.PORT || 1337,
+
+  // 音乐路径前缀
   musicPathPre: '/static/',
-  wallpaperPathPre: '/static/'
+
+  // 壁纸路径前缀
+  wallpaperPathPre: '/static/',
+
+  // 爬虫采集间隔时间（单位：毫秒）
+  scrapRate: 10 * 60 & 1000
 };
 
 module.exports = config;
