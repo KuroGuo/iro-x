13db736fd852eeb96e3412186827b5268c258b1d
KuroGuo <413298956@qq.com>
2014-12-07 00:02:17 +0800

    fix 内存泄露

--- static/js/wallpaper.js
+++ static/js/wallpaper.js
@@ -1,24 +1,26 @@
 ;(function (angular) { 'use strict';
   angular.module('iro.wallpaper', ['iro.services.wallpaper', 'iro.services.navbar', 'kScroll'])
-    .controller('WallpaperCtrl', ['$scope', 'wallpaper', 'navbar', '$window', '$document',
-      function ($scope, wallpaper, navbar, $window, $document) {
-        $scope.wallpapers = wallpaper.query();
+    .controller('WallpaperCtrl', ['$scope', 'Wallpaper', 'navbar', '$window', '$document',
+      function ($scope, Wallpaper, navbar, $window, $document) {
+        $scope.wallpapers = Wallpaper.query();
 
         $scope.liHeight = '16rem';
-        $window.addEventListener('resize', function () {
-          $scope.$apply(rewidth);
-        });
+        $window.addEventListener('resize', applyRewidth);
         rewidth.call($window);
 
         navbar.customBackgroundColor = 'rgba(51,51,51,1)';
         $scope.$on('$destroy', function () {
-          $window.removeEventListener('resize', rewidth);
+          $window.removeEventListener('resize', applyRewidth);
           navbar.customBackgroundColor = null;
         });
 
+        function applyRewidth() {
+          $scope.$apply(rewidth);
+        }
+
         function rewidth() {
           var rootFontSize = parseFloat($document.find('html').css('font-size'));
-          $scope.liWidth = 100 / Math.round($window.innerWidth / rootFontSize / (parseFloat($scope.liHeight) * 16 / 9)) + '%';
+          $scope.liWidth = 100 / Math.round($window.innerWidth / rootFontSize / (parseFloat($scope.liHeight) * 16 / 9)) + '%';  
         }
       }]);
 })(angular);
