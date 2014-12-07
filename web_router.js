e76c91aa4ca87110045325aa3a7111c74731cbe0
KuroGuo <413298956@qq.com>
2014-12-07 11:23:21 +0800

    新增资讯栏目相关接口

--- web_router.js
+++ web_router.js
@@ -4,11 +4,15 @@ var express = require('express');
 
 var music = require('./controllers/music');
 var wallpaper = require('./controllers/wallpaper');
+var news = require('./controllers/news');
 
 var router = express.Router();
 
 router.get('/music', music.all);
 
 router.get('/wallpaper', wallpaper.all);
+
+router.get('/news', news.query);
+router.get('/news/:id', news.get);
 
 module.exports = router;
